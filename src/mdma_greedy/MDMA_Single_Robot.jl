21f6bb464a12fc8b8d8d6b6f5b3ba2a4a8456f16
skylerh <skyler_hughes@outlook.com>
2022-09-22 12:02:17 -0600

    Time dependence complete

--- src/mdma_greedy/MDMA_Single_Robot.jl
+++ src/mdma_greedy/MDMA_Single_Robot.jl
@@ -29,44 +29,45 @@ struct MDPState
 end
 # Constructor for initial states
 MDPState(state) = MDPState(state, 1, nothing)
-MDPState(m::MDPState, s::State) = MDPState(s, m.depth + 1, m)
+MDPState(m::MDPState, s::State) = MDPState(s, m.depth + 1, nothing)
+MDPState(m::MDPState) = MDPState(m.state, m.depth+1, nothing)
+MDPState(m::MDPState, a::MDPState) = MDPState(a.state, m.depth + 1, nothing)
 
 # State and trajectory objects for convenience
 struct Grid
     width::Int64
     height::Int64
-    depth::Int64
-    states::Array{State,3}
-    transition_matrix::SparseMatrixCSC{Float64,Int64}
+    angle_divisions::Int64
+    horizon::Int64
+    states::Array{MDPState,4}
 
     # We will precompute some of the large objects that we use frequently
-    function Grid(width, height)
-        x = new(width, height, 8, get_states(width, height),
-                sparse([], [], Float64[], width*height, width*height)
-                )
+    function Grid(width, height, horizon)
+        x = new(width, height, 8, horizon, get_states(width, height, horizon))
         x
     end
 end
 get_states(g::Grid) = g.states
-function get_states(width::Int64, height::Int64)
-    states = Array{State,3}(undef, width, height, 8)
+function get_states(width::Int64, height::Int64, horizon::Int64)
+    states = Array{MDPState,4}(undef, width, height, 8, horizon)
     for i in CartesianIndices(states)
         r = i[1]
         c = i[2]
         d = i[3]
-        states[i] = UAVState(c,r,cardinaldir[d])
+        t = i[4]
+        states[i] = MDPState(UAVState(c,r,cardinaldir[d]), t, nothing)
     end
     states
 end
 
-dims(g::Grid) = (g.width, g.height, g.depth)
+dims(g::Grid) = (g.width, g.height, g.angle_divisions, g.horizon)
 num_states(g::Grid) = length(g.states)
 
 # States in grid should not have x or y lower than 1 or more than width/height
-state_to_index(g::Grid, s::MDPState) = (s.state.y, s.state.x, dir_to_index(s.state.heading))
+state_to_index(g::Grid, s::MDPState) = (s.state.y, s.state.x, dir_to_index(s.state.heading), s.depth)
 
 
-abstract type AbstractSingleRobotProblem <: MDP{MDPState, State} end
+abstract type AbstractSingleRobotProblem <: MDP{MDPState, MDPState} end
 
 struct SingleRobotMultiTargetViewCoverageProblem <: AbstractSingleRobotProblem
     grid::Grid
@@ -89,79 +90,38 @@ end
 get_states(model::SingleRobotMultiTargetViewCoverageProblem) = get_states(model.grid)
 horizon(x::AbstractSingleRobotProblem) = x.horizon
 
-function state_trajectory(state::MDPState)
-    ret = Array{State}(undef, state.depth)
 
-    ret[1] = state.state
+# POMDPs.transition(model::AbstractSingleRobotProblem, state::UAVState, action::UAVState) = transition(model, MDPState(state), action)
+function POMDPs.transition(model::AbstractSingleRobotProblem, state::MDPState, action::MDPState)
 
-    curr_state = state
-    for ii = 2:state.depth
-        curr_state = curr_state.prev
-        ret[ii] = curr_state.state
-    end
-
-    ret
-end
-
-POMDPs.transition(model::AbstractSingleRobotProblem, state::UAVState, action::UAVState) = transition(model, MDPState(state), action)
-function POMDPs.transition(model::AbstractSingleRobotProblem, state::MDPState, action::UAVState)
-
-    nbors = neighbors(model.grid, state, model.move_dist)
+    time = state.depth
+    nbors = neighbors(model, state, model.move_dist)
     if action in nbors
-        states = nbors
-        num_neighbors = length(nbors)
-
-        # Need to convert all neighbors to MDPStates
-        MDPStates = Vector{MDPState}(undef, 0)
-        for s in nbors
-            push!(MDPStates, MDPState(state,s))
-        end
-
-        # map(x->MDPState(state,action), nbors)
-        probs = Vector{Float64}(undef, num_neighbors)
-        push!(MDPStates, state)
-        push!(probs, 1.0)
         return SparseCat([MDPState(state, action)], [1.0])
     else
-        states = nbors
-        num_neighbors = length(nbors)
-
-        # Need to convert all neighbors to MDPStates
-        MDPStates = Vector{MDPState}(undef, 0)
-        for s in nbors
-            push!(MDPStates, MDPState(state,s))
-        end
-
-        # map(x->MDPState(state,action), nbors)
-        probs = Vector{Float64}(undef, num_neighbors)
-        push!(MDPStates, state)
-        push!(probs, 1.0)
-        println("TIME ", MDPStates[1].depth)
-        return SparseCat(MDPStates, probs)
+        return SparseCat([MDPState(state.state, state.depth + 1, nothing)], [1.0])
     end
 
 end
+
+
 function POMDPs.states(model::AbstractSingleRobotProblem)
-    uavstates = get_states(model)
-    mdpstates = Vector{MDPState}(undef, 0)
-    for s in uavstates
-        push!(mdpstates, MDPState(s))
-    end
-    mdpstates
+    get_states(model)
 end
+
 function POMDPs.actions(model::AbstractSingleRobotProblem)
     get_states(model.grid)
 end
 
 function POMDPs.actions(model::AbstractSingleRobotProblem, state::MDPState)
-    neighbors(model.grid, state.state, model.move_dist)
+    neighbors(model, state, model.move_dist)
 end
 
 function POMDPs.stateindex(model::AbstractSingleRobotProblem, s::MDPState)
-    cart = CartesianIndex(s.state.y, s.state.x, dir_to_index(s.state.heading))
+    cart = CartesianIndex(s.state.y, s.state.x, dir_to_index(s.state.heading), s.depth)
     grid = model.grid
     d = dims(grid)
-    lin = LinearIndices((1:d[1], 1:d[2], 1:d[3]))
+    lin = LinearIndices((1:d[1], 1:d[2], 1:d[3], 1:d[4]))
     return lin[cart]
 end
 # POMDPs.stateindex(model::AbstractSingleRobotProblem, state::MDPState) = stateindex(model, state.state)
@@ -169,38 +129,35 @@ end
 #     state.x * model.grid.width + state.y*model.grid.height + findall(x->x==state.heading, cardinaldir)[1]
 # end
 
-function POMDPs.actionindex(model::AbstractSingleRobotProblem, state::UAVState)
-    stateindex(model, MDPState(state))
+function POMDPs.actionindex(model::AbstractSingleRobotProblem, action::MDPState)
+    stateindex(model, action)
 end
 
 function dist_check(x1::Number,y1::Number, x2::Number,y2::Number, d::Number)
     (x2-x1)^2 + (y2-y1)^2 <= d^2
 end
 
-neighbors(grid::Grid, mstate::MDPState, d::Integer) = neighbors(grid, mstate.state, d)
+# neighbors(grid::Grid, mstate::MDPState, d::Integer) = neighbors(grid, mstate.state, d)
 # Compute neighbors within a certain distance of the state in the grid
-function neighbors(grid::Grid, state::State, d::Integer)::Vector{State}
-    actions = Vector{State}(undef, 0)
+function neighbors(model::AbstractSingleRobotProblem, state::MDPState, d::Integer)::Vector{MDPState}
+    actions = Vector{MDPState}(undef, 0)
     diff = d ÷ 2 + 1# Search within a square region around the object
     diff = Int64(diff)
-    for x = state.x-diff:state.x+diff
-        for y = state.y-diff:state.y+diff
-            if dist_check(state.x, state.y, x,y, d) && in_bounds(grid,x,y)
-                # for d in cardinaldir
-                #     new = UAVState(x,y,d)
-                #     if new != state
-                #         push!(actions, new)
-                #     end
-                # end
-                push!(actions, UAVState(x,y, ccw(state.heading)))
-                push!(actions, UAVState(x,y, cw(state.heading)))
-                # push!(actions, UAVState(x,y, state.heading))
-                # if x != state.x && y != state.y
-                push!(actions, state)
-                # push!(actions, UAVState(x,y, state.heading))
-                # end
+    grid = model.grid
+    uavstate = state.state
+    for x = uavstate.x-diff:uavstate.x+diff
+        for y = uavstate.y-diff:uavstate.y+diff
+            if dist_check(uavstate.x, uavstate.y, x,y, d) && in_bounds(grid,x,y)
+                if state.depth + 1 <= model.horizon
+                  push!(actions, MDPState(state, UAVState(x,y, ccw(uavstate.heading))))
+                  push!(actions, MDPState(state, UAVState(x,y, cw(uavstate.heading))))
+
+                end
             end
         end
+    end
+    if state.depth + 1 <= model.horizon
+        push!(actions, MDPState(state))
     end
     actions
 end
@@ -219,29 +176,30 @@ end
 # Make neighbors within a distance of grid
 # Have used a sparsematrix to represent transitions in prior work
 
-function solve_single_robot(problem::AbstractSingleRobotProblem,
-                            state::State)
+function solve_single_robot(problem::AbstractSingleRobotProblem)
 
-    solver = ValueIterationSolver(max_iterations=200, belres=1e-6, verbose=true)
+    solver = ValueIterationSolver(max_iterations=90, belres=1e-6, verbose=true)
     policy = solve(solver, problem)
 
-    action, info = action_info(policy, MDPState(state))
-    print(action)
-    print(info)
+    # action, info = action_info(policy, MDPState(state))
+    # print(action)
+    # print(info)
     return policy
 end
 
-function POMDPs.reward(model::AbstractSingleRobotProblem, state::MDPState, action::State)
+function POMDPs.reward(model::AbstractSingleRobotProblem, state::MDPState, action::MDPState)
     # Want to just give a reward value if you detect an object
     reward = 0
-    time = state.depth
-    for targets in eachrow(model.target_trajectories)
-        for t in targets
-            if detectTarget(action, t, model.sensor)
-                reward += 5
-            end
+    time = action.depth
+    # println("r ", time)
+    targets = model.target_trajectories[time,:]
+    # for targets in eachrow(model.target_trajectories)
+    for t in targets
+        if detectTarget(action.state, t, model.sensor)
+            reward += 5
         end
     end
+    # end
     reward
 end
 
@@ -256,7 +214,6 @@ POMDPs.initialstate(model::AbstractSingleRobotProblem) = MDPState(UAVState(1,1,:
 
 function generate_target_trajectories(grid::Grid, horizon::Integer, initial::Vector{Target})::Array{Target,2}
     trajectory = Array{Target, 2}(undef, horizon, length(initial))
-    ystart = grid.height
     yend = 4
     x = grid.width - 5
     trajectory[1,:] = initial
@@ -264,7 +221,7 @@ function generate_target_trajectories(grid::Grid, horizon::Integer, initial::Vec
     for i = 2:horizon
         new = Vector{Target}(undef,0)
         for t in current
-            push!(new, Target(t.x,t.y+((yend-ystart)/i), 0))
+            push!(new, Target(t.x,t.y+(-0.5), 0))
         end
         current = new
         trajectory[i,:] = current
@@ -272,22 +229,6 @@ function generate_target_trajectories(grid::Grid, horizon::Integer, initial::Vec
     return trajectory
 end
 
-function bruh()
-    sensor = ViewConeSensor(pi/2, 3)
-    targets = Vector{Target}(undef, 0)
-    push!(targets, Target(1,2,0))
-    push!(targets, Target(1,3,0))
-    push!(targets, Target(2,3,0))
-
-    grid = Grid(20,20)
-    horizon = 30
-    model = SingleRobotMultiTargetViewCoverageProblem(grid, sensor, horizon, targets, 3)
-
-    # print(model.target_trajectories)
-    # print(transition(model, MDPState(UAVState(1,1,:N)), UAVState(1,2,:N)))
-    # print(transition(model, UAVState(1,1,:N), UAVState(1,2,:N))
-    println("test ", reward(model, MDPState(UAVState(1,1,:N)), UAVState(1,1,:N)))
-end
 @testset "single_robot_planner" begin
     sensor = ViewConeSensor(pi/2, 3)
     targets = Vector{Target}(undef, 0)
@@ -295,16 +236,16 @@ end
     push!(targets, Target(1,3,0))
     push!(targets, Target(2,3,0))
 
-    grid = Grid(20,20)
-    horizon = 30
+    horizon = 4
+    grid = Grid(20,20, horizon)
     model = SingleRobotMultiTargetViewCoverageProblem(grid, sensor, horizon, targets, 3)
 
     # print(model.target_trajectories)
     # print(transition(model, MDPState(UAVState(1,1,:N)), UAVState(1,2,:N)))
     # print(transition(model, UAVState(1,1,:N), UAVState(1,2,:N))
     # println("test ", reward(model, MDPState(UAVState(1,1,:N)), UAVState(1,1,:N)))
-    @test reward(model, MDPState(UAVState(1,1,:N)), UAVState(1,1,:N)) == 15
+    @test reward(model, MDPState(UAVState(1,1,:N)), MDPState(UAVState(1,1,:N))) == 15
     targets[3] = Target(10,10,0)
     model = SingleRobotMultiTargetViewCoverageProblem(grid, sensor, horizon, targets, 3)
-    @test reward(model, MDPState(UAVState(1,1,:N)), UAVState(1,1,:N)) == 10
+    @test reward(model, MDPState(UAVState(1,1,:N)), MDPState(UAVState(1,1,:N))) == 10
 end
