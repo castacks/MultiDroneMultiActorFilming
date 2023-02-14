2825ed7c9f8d6a82eae5a550d6a6e3942fa8c6d9
shastro <guitarshughes@hotmail.com>
2023-02-14 02:07:55 -0700

    JSON parsing in Julia

--- src/mdma_greedy/MDMA_Interface.jl
+++ src/mdma_greedy/MDMA_Interface.jl
@@ -1,18 +1,32 @@
 # This file contains code for interfacing with Airsim
 
 import JSON
+using Test
+using MDMA
 
-function targets_from_json(filename::string)::Array{Target, 2}
+function configs_from_file(filename::String, move_dist::Number)::MultiDroneMultiActorConfigs
 
-   f = open(filename)
-   json_root = JSON.parse(f.read())
-   f.close()
+    json_root = JSON.parsefile(filename)
 
-   println!(json_root)
+    scene = json_root["scene"]
+    scale = scene["scale"]
+    blend_file = json_root["filename"]
+    horizon = json_root["num_frames"]
+    num_targets = json_root["num_targets"]
+    robot_fovs = json_root["robot_fovs"]
+    num_robots = json_root["num_robots"]
+
+    generate_target_trajectories = Array{Target, 2}(nothing, num_targets, horizon)
+    for loc_pos in json_root["actor_positions"]
+        loc = loc_pos["location"]
+        rot = loc_pos["rotation"]
+
+
 
 end
 
 
 @testset "test_file_parse" begin
-    # targets_from_file("./src/mdma_greedy/trajectory1.txt")
+    configs_from_file("../../blender/four_split_data.json", 3)
+    println(pwd())
 end
