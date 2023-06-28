37c35023b14da314f87d80050fe508674c6fba47
shastro <guitarshughes@hotmail.com>
2023-06-28 08:26:22 -0600

    Add myopic planner

--- src/mdma_greedy/MDMA_Experiment.jl
+++ src/mdma_greedy/MDMA_Experiment.jl
@@ -14,6 +14,50 @@ export blender_render_all_experiments, evaluate_all_experiments
 struct GreedyPlanner end
 struct FormationPlanner end
 struct AssignmentPlanner end
+struct MyopicPlanner end
+
+function run_experiment(
+    experiment_name::String,
+    path_to_experiments::String,
+    _::MyopicPlanner,
+)
+    # Set model parameters
+    focal_length = [20.0, 20.0]#mm
+    resolution = [3840.0, 2160.0]
+    lens_dim = [35.00, 24.00] #mm
+    pitch = -0.3490655 # if you change this also change animate_cameras.py
+    cutoff = 100.0
+    # sensor = MDMA.PinholeCameraModel(focal_length, resolution, lens_dim, 0.0, pitch, cutoff)
+    sensor = MDMA.ViewConeSensor(pi / 2, cutoff)
+    move_dist = 3
+
+    multi_configs = configs_from_file(
+        "$(path_to_experiments)/$(experiment_name)/$(experiment_name)_data.json",
+        experiment_name,
+        move_dist,
+    )
+
+    multi_configs.sensor = sensor
+    # model = MDMA.SingleRobotMultiTargetViewCoverageProblem(grid, sensor, horizon, target_trajectories, move_dist);
+    robot_states = map(
+        x -> MDMA.random_state(multi_configs.horizon, multi_configs.grid),
+        1:multi_configs.num_robots,
+    )
+    multi_problem = MDMA.MultiRobotTargetCoverageProblem(robot_states, multi_configs)
+
+    # Solving output
+    println("Solving Solution for $(experiment_name) using GreedyPlanner")
+    solution = solve_myopic(multi_problem)
+
+    # Save the solution
+    MDMA.save_solution(
+        experiment_name,
+        path_to_experiments,
+        "greedy",
+        solution,
+        multi_configs,
+    )
+end
 
 function run_experiment(
     experiment_name::String,
@@ -113,11 +157,7 @@ function run_experiment(
     # Initial States    
     # TODO: Random phase angle
     # Instead of starting states
-    robot_states = map(
-        x -> MDMA.random_state(multi_configs.horizon, multi_configs.grid),
-        1:multi_configs.num_robots,
-    )
-    formation_configs = formation_from_multi(10.0, robot_states, multi_configs)
+    formation_configs = formation_from_multi(15.0, randn(), multi_configs)
 
     println("Solving Solution for $(experiment_name) using FormationPlanner")
     solution = solve_formation(formation_configs)
@@ -135,6 +175,8 @@ struct ExperimentsConfig
     experiments::Vector{String}
 end
 
+
+ExperimentsConfig(path_to_experiments::String) = ExperimentsConfig(path_to_experiments, ["cluster", "cross_mix", "four_split", "split_and_join", "spreadout_group", "track_runners", "priority_runners", "priority_speaker"])
 
 # Experiment Steps
 #  - Generate $(experiment_name)_data.json using a blender call
